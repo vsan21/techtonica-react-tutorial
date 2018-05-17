af55a7b3cd9c8c5397f9b16bbfbb2310a59df3d1
David Moore <davidimoore@gmail.com>
2018-05-16 17:33:04 -0700

    add WordSearch container

--- client/src/components/WordSearch/index.js
+++ client/src/components/WordSearch/index.js
@@ -5,35 +5,38 @@ export default class WordSearch extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      word: null,
+      word: "",
     };
   }
+
   handleChange = (event) => {
     this.setState({ word: event.target.value })
   };
 
   render() {
     return (
-      <div
-        className={ `SearchFormContainer` }
-      >
-        <form
-          className={ `SearchForm` }
+      <div className={ `WordSearch` }>
+        <div
+          className={ `SearchFormContainer` }
         >
-          <input
-            className={ `WordTextInput` }
-            type="text"
-            name="words"
-            value={ this.state.word }
-            onChange={ this.handleChange }
-          />
+          <form
+            className={ `SearchForm` }
+          >
+            <input
+              className={ `WordTextInput` }
+              type="text"
+              name="words"
+              value={ this.state.word }
+              onChange={ this.handleChange }
+            />
 
-          <input
-            className={ `Submit` }
-            type="submit"
-            value="Submit"
-          />
-        </form>
+            <input
+              className={ `Submit` }
+              type="submit"
+              value="Submit"
+            />
+          </form>
+        </div>
       </div>
     )
   }
